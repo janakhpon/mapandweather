e8361066cd1ee46ea22a2ec67ed8b2cd2cf0d17c
janakhpon <jnovaxer@gmail.com>
2019-09-17 22:23:53 +0630

    Updates

---
+++ precache-manifest.3f70bb7bd284e0c44022a5ca2a5d10e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd7161f79c69ac035e1825612b1f79e1",
+    "url": "/mapandweather/index.html"
+  },
+  {
+    "revision": "617d43a930bd24f5d820",
+    "url": "/mapandweather/static/css/main.0352a8c2.chunk.css"
+  },
+  {
+    "revision": "05b498b7698c6305353d",
+    "url": "/mapandweather/static/js/2.d7a1e0d2.chunk.js"
+  },
+  {
+    "revision": "617d43a930bd24f5d820",
+    "url": "/mapandweather/static/js/main.73ae798f.chunk.js"
+  },
+  {
+    "revision": "ad317cbf5cb241756ba4",
+    "url": "/mapandweather/static/js/runtime~main.9f66661b.js"
+  }
+]);
--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers} from 'redux';
-import weatherReducer from './weatherReducer';
-
-const rootReducer = combineReducers({
-    weather:weatherReducer
-})
-
-
-export default rootReducer;
